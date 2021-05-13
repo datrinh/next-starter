8e26f1068708b9604ddefc13579c505c50287e1d
Duc Anh Trinh <ducanh.trinh@hotmail.de>
2021-05-13 10:38:06 +0200

    update eslintrc for next

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,10 +13,27 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      extends: ['plugin:react/recommended', 'airbnb-typescript'],
+      plugins: ['react', '@typescript-eslint'],
+      rules: {
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+      },
+    },
   ],
-  rules: {
-  },
 };
